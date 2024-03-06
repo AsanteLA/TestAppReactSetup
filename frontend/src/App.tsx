606eb8ac92b74ea704bc99829dbd34398577583e
Asante Laryea Akrong <kal79@byu.edu>
2024-03-05 20:40:04 -0700

    React updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,67 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { NOTIMP } from "dns";
 
+interface Bandprops {
+  name: string;
+  members: string;
+  formed: number;
+}
+
+const bandNames = [
+  {
+    name: "The Beastie Boys",
+    members: "Mike D, MCA, Ad-Rock",
+    formed: 1979,
+  },
+  {
+    name: "Wolfmother",
+    members: "Andrew Stockdale, Hamish Rosser, Bobby Poulton",
+    formed: 2004,
+  },
+  {
+    name: "Cream",
+    members: "Eric Clapton, Ginger Baker, Jack Bruce",
+    formed: 1966,
+  },
+  {
+    name: "Nirvana",
+    members: "Kurt Cobain, Krist Novoselic, Dave Grohl",
+    formed: 1987,
+  },
+];
+
+function Welcome() {
+  return <h1>The best music trios ever!</h1>;
+}
+
+class Band extends React.Component<Bandprops> {
+  render() {
+    const oneBand = this.props;
+    return (
+      <div>
+        <img />
+        <h2>{oneBand.name}</h2>
+        <h3>Members: {oneBand.members}</h3>
+        <h3>Formed: {oneBand.formed}</h3>
+      </div>
+    );
+  }
+}
+function BandList() {
+  return (
+    <div>
+      {bandNames.map((oneBand) => (
+        <Band {...oneBand} />
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
